f88b414fdebbc75cb3c3c1b6878974aa4bc150f8
KastriotR <kast.12@hotmail.com>
2014-06-23 09:20:27 -0700

    LK

---
+++ LK/LK/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LK
+//
+//  Created by Kastriot Rama on 6/23/14.
+//  Copyright (c) 2014 Kastriot Rama. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
